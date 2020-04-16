e35f1a37a982c177ac1b7c2ff40391f7ee0b2bf5
Nik Clayton <nik.clayton@dfinity.org>
2020-04-16 15:55:19 +0200

    Note undocumented nature of mergeable_state field

--- src/canMerge.ts
+++ src/canMerge.ts
@@ -3,6 +3,10 @@ import Octokit = require('@octokit/rest')
 export function canMergeByMergeable(pr: Octokit.PullsGetResponse) {
     return pr.mergeable
 }
+
+// Note, mergeable_state is an undocumented field
+// (per https://github.community/t5/GitHub-API-Development-and/PullRequest-mergeable-state-possible-values/td-p/21943)
+// with some possible values noted in https://github.com/octokit/octokit.net/issues/1763
 export function canMergeByMergeableState(pr: Octokit.PullsGetResponse) {
     return pr.mergeable_state === 'clean' || pr.mergeable_state === 'unstable'
 }
